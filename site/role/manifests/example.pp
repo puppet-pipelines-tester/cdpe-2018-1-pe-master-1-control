3ee27d3dbe76fc66ad111e7d461edbad89f7181a
Nate Wolfe <nwolfe@puppet.com>
2018-08-27 13:56:29 -0700

    (CDPE-425) Changes resources in staging

--- site/role/manifests/example.pp
+++ site/role/manifests/example.pp
@@ -1,9 +1,3 @@
 class role::example {
-  notify {'This is production Sparta!':}
-
-  file { 'some-file':
-       path => '/tmp/some-file',
-       content => 'Hi there',
-       ensure => present,
-  }
+  notify {'This is staging Sparta!':}
 }
