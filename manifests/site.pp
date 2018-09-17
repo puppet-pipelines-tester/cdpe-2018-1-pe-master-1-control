607f1ac9803be4b7087b667cf2e21c8bebd42404
CDPE Unit Tests <pipelines-team@puppet.com>
2018-09-17 16:49:27 +0000

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -51,7 +51,11 @@ node default {
   
 }
 
-node cdpe-ia-agent-5.delivery.puppetlabs.net {
+node 'cdpe-ia-agent-5.delivery.puppetlabs.net' {
   warning("this is a warning from site.pp")
   deprecation("$fake::key", "This fake key has been deprecated for the logs")
+  file { '/tmp/impact_analysis_staging_test':
+    ensure => file,
+    content => 'this is from site.pp'
+  }
 }
