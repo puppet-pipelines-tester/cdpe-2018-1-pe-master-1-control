9b2b22015876b24488c465926832e45711e0ba6d
CDPE Unit Tests <pipelines-team@puppet.com>
2018-09-17 23:38:13 +0000

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -41,6 +41,7 @@ node default {
   # Example:
   #   class { 'my_class': }
   notify { "Hello world!": message => "I am in the ${environment} environment"}
+  include cd4pe_tests
 }
 
 node 'cdpe-ia-agent-5.delivery.puppetlabs.net' {
