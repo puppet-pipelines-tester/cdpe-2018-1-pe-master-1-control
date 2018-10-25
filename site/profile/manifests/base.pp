df3d486280dd13ba0344a31e3960bbf032d7467e
ericwilliamson <eric.williamson@puppet.com>
2018-10-25 11:45:27 -0700

    add type with sensitive data

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -2,4 +2,8 @@ class profile::base {
   #the base profile should include component modules that will be on all nodes
   notify {"profile::base": }
 
+  file { "/tmp/sensitive":
+    ensure => file,
+    content => Sensitive("totaly sensitive")
+  }
 }
