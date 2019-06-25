7f04d5b657eb9996227da222f3bc6b0e8db2ef2e
ericwilliamson <eric.williamson@puppet.com>
2019-06-24 17:10:11 -0700

    (maint) Add clamps for more resource / nodes tests

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -6,4 +6,35 @@ class profile::base {
     ensure => file,
     content => Sensitive("totaly not sensitive")
   }
+
+  class { 'clamps': 
+    num_dynamic_files => 1,
+    num_static_files  => 1,
+  }
+
+  class { 'clamps::agent': 
+    environment => 'impact_analysis_tests_staging',
+    num_dynamic_files => 1,
+    num_static_files  => 1,
+  }
+
+  file { "/home/root":
+    ensure => directory
+  }
+
+  if $facts['id'] =~ /^user/ {
+    class { 'clamps': 
+      num_dynamic_files => 1,
+      num_static_files  => 1,
+    }
+  } else {
+    class { 'clamps::agent': 
+    }
+  }
+
+  file { '/tmp/diff-file-params':
+    path => "/tmp/${facts['id']}",
+    content => 'diff on each user',
+  }
+>>>>>>> f8d0eb3... (maint) Add clamps for more resource / nodes tests
 }
