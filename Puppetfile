09fcb3a1a0c492d3929db5dc0f7f8c7ae0d1af50
ericwilliamson <eric.williamson@puppet.com>
2019-06-24 17:10:11 -0700

    (maint) Add clamps for more resource / nodes tests

--- Puppetfile
+++ Puppetfile
@@ -3,6 +3,9 @@ mod 'puppetlabs-stdlib', '4.25.1'
 mod 'puppetlabs-inifile', '2.3.0'
 mod 'puppetlabs-hocon', '1.0.1'
 mod 'puppetlabs-concat', '4.2.1'
+mod 'clamps',
+  :git => 'git@github.com:puppetlabs/clamps.git',
+  :ref => 'master'
 mod 'cd4pe',
   :git => 'git@github.com:puppetlabs/puppetlabs-cd4pe.git',
   :ref => 'master'
--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -6,4 +6,23 @@ class profile::base {
     ensure => file,
     content => Sensitive("totaly sensitive")
   }
+
+  file { "/home/root":
+    ensure => directory
+  }
+
+  if $facts['id'] =~ /^user/ {
+    class { 'clamps': 
+      num_dynamic_files => 1,
+      num_static_files  => 1,
+    }
+  } else {
+    class { 'clamps::agent': 
+    }
+  }
+
+  file { '/tmp/diff-file-params':
+    path => "/tmp/${facts['id']}",
+    content => 'diff on each user',
+  }
 }
