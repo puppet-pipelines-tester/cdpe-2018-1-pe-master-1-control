a5afd0f75f0f479bbb56e6969b81098d938c52d3
ericwilliamson <eric.williamson@puppet.com>
2019-09-10 12:05:07 -0700

    (maint) Add targetspec

--- modules/deployments/plans/test.pp
+++ modules/deployments/plans/test.pp
@@ -1,11 +1,12 @@
 plan deployments::test(
+  TargetSpec $nodes,
   Boolean $should_fail = false,
 ) {
   if($should_fail) {
     fail("The plan deployment failed!")
   }
 
-  $target = get_targets($host)[0]
+  $target = get_targets($nodes)[0]
   $target.set_var('newly_provisioned', true)
   $targetvars = $target.vars
   run_command("echo 'Vars for ${host}: ${$targetvars}'", $host)
