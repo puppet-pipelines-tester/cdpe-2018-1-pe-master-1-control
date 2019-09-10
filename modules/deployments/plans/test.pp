ea3f6315afced6475cd6c74668312dfa0982c4c7
ericwilliamson <eric.williamson@puppet.com>
2019-09-10 15:25:05 -0700

    (maint) simplify the test plan

--- modules/deployments/plans/test.pp
+++ modules/deployments/plans/test.pp
@@ -1,13 +1,9 @@
 plan deployments::test(
-  TargetSpec $nodes,
   Boolean $should_fail = false,
 ) {
   if($should_fail) {
     fail("This is a forced failure from the test plan!")
   }
 
-  $target = get_targets($nodes)[0]
-  $target.set_var('newly_provisioned', true)
-  $targetvars = $target.vars
-  return run_command("echo 'Vars for ${$target}: ${$targetvars}'", $target)
+  return "success message from the test plan"
 }
