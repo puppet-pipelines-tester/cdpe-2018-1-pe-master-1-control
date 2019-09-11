3d2f6a36998d8e4a1c4804be8771ee51565514b0
ericwilliamson <eric.williamson@puppet.com>
2019-09-10 17:48:24 -0700

    (maint) add in targetspec as optional

--- modules/deployments/plans/test.pp
+++ modules/deployments/plans/test.pp
@@ -1,10 +1,15 @@
 plan deployments::test(
+  Optional[TargetSpec] $nodes = undef,
   Boolean $should_fail = false,
 ) {
   if($should_fail) {
     fail("This is a forced failure from the test plan!")
   }
   $test_env_var = system::env('TEST_ENV_VAR')
+  if($nodes) {
+    return run_command("hostname", $nodes)
+  } else {
+    return "success message from the test plan. env_var = ${test_env_var}"
+  }
 
-  return "success message from the test plan. env_var = ${test_env_var}"
 }
