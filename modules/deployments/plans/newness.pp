1e02931a9cd73ac4b5ccdc769eb65cde49ad9667
ericwilliamson <eric.williamson@puppet.com>
2019-10-06 20:40:25 -0700

    (maint) add a second plan for testing

---
+++ modules/deployments/plans/newness.pp
@@ -0,0 +1,17 @@
+plan deployments::newness(
+  Optional[TargetSpec] $nodes = undef,
+  Boolean $should_fail = false,
+  String boo,
+  bar
+) {
+  if($should_fail) {
+    fail("This is a forced failure from the test plan!")
+  }
+  $test_env_var = system::env('TEST_ENV_VAR')
+  if($nodes) {
+    return run_command("hostname", $nodes)
+  } else {
+    return "success message from the test plan. env_var = ${test_env_var}"
+  }
+
+}
