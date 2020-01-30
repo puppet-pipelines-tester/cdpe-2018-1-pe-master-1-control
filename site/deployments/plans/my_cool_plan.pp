4a30b8b45f051250954e0f83ff4c4d35eac5bd13
puppet-pipelines-tester <40370030+puppet-pipelines-tester@users.noreply.github.com>
2020-01-30 15:37:50 -0800

    Create my_cool_plan.pp

---
+++ site/deployments/plans/my_cool_plan.pp
@@ -0,0 +1,17 @@
+# This deployment policy will deploy a source commit to the Puppet environment
+# associated with the Deployment's configured Node Group. It will then run Puppet
+# on all nodes in the environemnt.
+#
+# @summary This deployment policy will deploy a source commit to the Puppet environment
+#          associated with the Deployment's configured node group and then run Puppet.
+#
+# @param max_node_failure
+#     The number of allowed failed Puppet runs that can occur before the Deployment will fail
+# @param noop
+#     Indicates if the Puppet run should be a noop.
+plan cd4pe_deployments::direct (
+  Integer $max_node_failure = 0,
+  Boolean $noop = false,
+) {
+  return 'my cool plan is very cool'
+}
